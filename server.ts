811be4aad813641babbade544c406b0761b866a8
thirupathi <thirupathidheeravath2@gmail.com>
2026-04-09 00:26:30 +0530

    fix duplicate listen

--- server.ts
+++ server.ts
@@ -53,9 +53,6 @@ app.use(cors({
 }));
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
 app.options("*", cors());
 
 app.use(express.json());
